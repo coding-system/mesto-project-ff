ab85ff275eb36e4dfff995a20530647b673f3c9b
Kirill Sheyanov <amocodingsystem@gmail.com>
2024-06-03 13:43:06 +0300

    asda

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,60 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template');//Темплейт
-const cardImage = cardTemplate.querySelector('.card__image');//Картинка темплйте
-const cardTitle = cardTemplate.querySelector('.card__title');//Заголовок темплейта
-// @todo: DOM узлы
-const placesList = document.querySelector('.places__list');//Грид с карточками
-const AddButton = document.querySelector('.profile__add-button');//Кнопка с плюсиком
-const DeliteButton = cardTemplate.querySelector('.card__delete-button');//Иконка "удалить карточку"
 
-const NewCard = document.querySelector('.popup_type_new-card');//Диалог с добавлением новой карточки
-const NewCardTitle = NewCard.querySelector('.popup__input_type_card-name');//Инпут с названием карточки
-const NewCardImage = NewCard.querySelector('.popup__input_type_url');//Инпут с ссылкой на картинку
-const NewCardSubmit = NewCard.querySelector('.popup__button');//Кнопка сохранить
+const cardTemplate = document.querySelector("#card-template").content; //Тэмплейт
+
+// const cardTitle = cardItem.querySelector('.card__title');
+
+// @todo: DOM узлы
+
+const content = document.querySelector(".content"); //Тэг main
+const cardsList = content.querySelector(".places__list"); //Грид с карточками
 
 // @todo: Функция создания карточки
-function addCard()
 
+function addCard(deleteCard) {
+   initialCards.forEach(function (item) {
+      const cardItem = cardTemplate
+         .querySelector(".places__item")
+         .cloneNode(true); //Карточка списка
+      const cardTitle = cardItem.querySelector(".card__title"); //Заголовок карточки
+      const cardImage = cardItem.querySelector(".card__image"); //Картинка карточки
+      const cardAlt = cardItem.querySelector(".card__title"); //Alt картинки
+      const deleteButton = cardItem.querySelector(".card__delete-button"); //Кнопка удаления карточки
+
+      cardImage.src = item.link;
+      cardAlt.alt = item.name;
+      cardTitle.textContent = item.name;
+      cardsList.append(cardItem);
+
+      deleteButton.addEventListener("click", () => {
+         deleteCard(cardItem);
+      });
+   });
+}
 // @todo: Функция удаления карточки
 
+function deleteCard(cardItem) {
+   cardItem.remove();
+}
 // @todo: Вывести карточки на страницу
 
+addCard(deleteCard);
+
 // placesList.append(cardTemplate);
+
+// Кнопки открытия и закрытия попапа
+// const NewCard = document.querySelector(".popup_type_new-card"); //Диалог с добавлением новой карточки
+// const NewCardSave = NewCard.querySelector(".popup__button"); //Кнопка сохранить
+// const NewCardClose = NewCard.querySelector(".popup__close"); //Крестик чтобы закрыть
+// const AddButton = content.querySelector(".profile__add-button"); //Кнопка с плюсиком
+
+// AddButton.addEventListener("click", openPopup);
+// NewCardClose.addEventListener("click", closePopup);
+
+// function openPopup() {
+//    NewCard.classList.add("popup_is-opened");
+// }
+
+// function closePopup() {
+//    NewCard.classList.remove("popup_is-opened");
+// }
